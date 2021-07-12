55b064bbd8481020bbdec2d2506f54b58661c180
Samantha Medeiros <samanthadmedeiros@outlook.com>
2021-07-12 12:39:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Projeto final, Teoria da Computação, tema 1
 # Samantha Medeiros e Christopher Tavares
-# 11 de julho de 2021
+# 12 de julho de 2021
 
 import re
 import requests
@@ -11,40 +11,45 @@ from exibir_links_externos import exibir_links_externos
 
 # Validando se a URL informada pertence ao domínio da Wikipedia
 while(True):
-    # wikipediaURL = input("Informe a URL do artigo da Wikipedia: ")
-    wikipediaURL = "https://pt.wikipedia.org/wiki/Adventure_Time"
+    wikipediaURL = input("Informe a URL do artigo da Wikipedia: ")
+    # wikipediaURL = "https://pt.wikipedia.org/wiki/Adventure_Time"
 
-    # Validando a URL com Regular Expressions
-    formato_aceito = re.compile('(https)\:\/\/(..)\.(wikipedia)\.(org)\/(wiki)\/(\S*)')
+    # Formato aceito: qualquer url do domínio da Wikipedia que esteja em português (pt)
+    formato_aceito = re.compile('(https)\:\/\/(pt)\.(wikipedia)\.(org)\/(wiki)\/(\S*)')
     url_aceita = formato_aceito.match(wikipediaURL)
 
     if url_aceita:
-        print("Você informou um endereço válido de artigo da Wikipedia.")
+        print("Você informou um endereço válido de um artigo em português da Wikipedia.")
         break
     else:
-        print("Esta URL não pertence ao domínio da Wikipedia. Tente novamente.")
+        print("Esta URL não pertence ao domínio da Wikipedia ou o artigo não está em português. Tente novamente.")
 
 response = requests.get(wikipediaURL) # realizando requisição para acessar a página
 
 # Obtendo o HTTP Status Code
 print("Status code:", response.status_code)
 
-# fazendo a conversão do objeto para fazer uma busca dentro do conteúdo HTML
-conteudoWiki = response.content
-artigoWiki = BeautifulSoup(conteudoWiki, 'html.parser')
+# variáveis que serão utilizadas nas buscas
+conteudoWiki = response.content # tipo bytes
+artigoWiki = BeautifulSoup(conteudoWiki, 'html.parser') # conversão para um objeto beautiful soup
+
 # Exibindo o tema do artigo
 assunto = artigoWiki.find('h1', {"class": re.compile("^firstHeading")})
-print(f"Estamos visitando uma página da Wikipedia sobre: {assunto.get_text()}")
+print(f"Bem-vindo! Estamos acessando um artigo da Wikipedia sobre: {assunto.get_text()}.")
 
+# Menu de opções
 op = True
 while(op):
-    print("BEM-VINDO!")
+    print("MENU")
     print("1 - exibir tópicos")
     print("2 - exibir imagens")
     print("3 - exibir links externos")
     print("0 - sair")
-    op = input("Por favor informe uma opção: ")
 
+    op = input("Por favor informe uma opção: ")
+    print()
+    
+    # Chamando as funções que se encontram em arquivos externos
     if(op == "1"):
         exibir_topicos(artigoWiki)
     elif(op == "2"):
@@ -52,10 +57,10 @@ while(op):
     elif(op == "3"):
         exibir_links_externos(conteudoWiki)
     elif(op == "0"):
-        print("Obrigado por usar o nosso programa")
+        print("Obrigado por usar o nosso programa.")
         op = False
         break
     else:
         print("Opção indisponivel ou inexistente, tente novamente.")
-
-print("Fim do programa :D")
+        print()
+# fim do programa
